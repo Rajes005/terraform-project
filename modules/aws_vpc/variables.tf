0c3533b7460ffeac9fc2f589dc579eaa4da06a49
Rajes005 <37709185+Rajes005@users.noreply.github.com>
2023-07-08 01:03:41 +0530

    Create variables.tf

---
+++ modules/aws_vpc/variables.tf
@@ -0,0 +1,11 @@
+#In aws_vpc for variable.tf
+variable "vpc_cidr"{
+  type = string
+  default = "172.16.0.0/16"
+}
+variable "vpc_name"{
+  type = map(any)
+  default = {
+    "Name" = "tf-example"
+  }
+}
