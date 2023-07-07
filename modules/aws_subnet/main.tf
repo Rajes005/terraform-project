747931750c535dd17e41dc53ffea64d0855356f3
Rajes005 <37709185+Rajes005@users.noreply.github.com>
2023-07-08 01:04:14 +0530

    Update main.tf

--- modules/aws_subnet/main.tf
+++ modules/aws_subnet/main.tf
@@ -1,1 +1,8 @@
+#In aws_subnet for main.tf
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = var.vpc_id
+  cidr_block        = var.cidr_block
+  #availability_zone = "us-west-2a"
 
+  tags = var.subnet_name
+}
