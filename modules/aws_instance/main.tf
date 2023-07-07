dff0cb1d518d637bf5e5365baed220c93e7a6ee8
Rajes005 <37709185+Rajes005@users.noreply.github.com>
2023-07-08 01:09:12 +0530

    Update main.tf

--- modules/aws_instance/main.tf
+++ modules/aws_instance/main.tf
@@ -1,1 +1,12 @@
+#In aws_instance for main.tf
+resource "aws_instance" "foo" {
+  ami           = var.instance_ami
+  instance_type = var.instance_type
 
+  network_interface{
+    network_interface_id = var.nic_id
+    device_index         = 0
+  }
+
+  tags = var.instance_name
+}
