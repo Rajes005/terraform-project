41731e3cea22b980360b9470e3221e1a2ef4f453
Rajes005 <37709185+Rajes005@users.noreply.github.com>
2023-07-08 12:48:22 +0530

    Update main.tf with for_each

--- main.tf
+++ main.tf
@@ -21,20 +21,36 @@ module "nic" {
   private_ips = var.private_ips
   nic_name = var.nic_name
 }
-module "instance" {
-  source = "./modules/aws_instance"
-  instance_name = var.instance_name
-  instance_type = var.instance_type
-  instance_ami = var.instance_ami
-  nic_id = module.nic.nic_id
+#module "instance" {
+#  source = "./modules/aws_instance"
+#  instance_name = var.instance_name
+#  instance_type = var.instance_type
+#  instance_ami = var.instance_ami
+#  nic_id = module.nic.nic_id
+#}
+#module "instance_east_1" {
+#  source = "./modules/aws_instance"
+#  providers = {
+#    aws = aws.east-1
+#  }
+#  instance_name = var.instance_name
+#  instance_type = var.instance_type
+#  instance_ami = var.instance_ami
+#  nic_id = module.nic.nic_id
+#}
+module "nic" {
+  source   = "./modules/aws_nic"
+  for_each = var.instance_config
+
+  subnet_id   = module.subnet.subnet_id
+  private_ips = each.value.private_ips
+  nic_name    = each.value.nic_name
 }
-module "instance_east_1" {
-  source = "./modules/aws_instance"
-  providers = {
-    aws = aws.east-1
-  }
-  instance_name = var.instance_name
-  instance_type = var.instance_type
-  instance_ami = var.instance_ami
-  nic_id = module.nic.nic_id
+module "instance" {
+  source        = "./modules/aws_instance"
+  for_each      = var.instance_config
+  instance_name = each.value.instance_name
+  instance_type = each.value.instance_type
+  instance_ami  = each.value.instance_ami
+  nic_id        = module.nic[each.key].nic_id
 }
